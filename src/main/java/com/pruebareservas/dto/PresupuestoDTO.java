27d8ecb188a51f86056ba27b90cee3ec83b1f57c
ElAles19 <alexanderv1901@gmail.com>
2026-05-02 18:00:06 -0500

    Sprint 2

---
+++ src/main/java/com/pruebareservas/dto/PresupuestoDTO.java
@@ -0,0 +1,14 @@
+package com.pruebareservas.dto;
+
+public class PresupuestoDTO {
+
+    private double total;
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
