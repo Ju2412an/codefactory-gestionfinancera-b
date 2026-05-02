03f49350d1193e776716b1deb645ce70f223e560
ElAles19 <alexanderv1901@gmail.com>
2026-05-02 18:01:00 -0500

    Sprint 2

---
+++ src/main/java/com/pruebareservas/repository/MovimientoRepository.java
@@ -0,0 +1,7 @@
+package com.pruebareservas.repository;
+
+import com.pruebareservas.entity.MovimientoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovimientoRepository extends JpaRepository<MovimientoEntity, Long> {
+}
---
+++ src/main/java/com/pruebareservas/repository/PresupuestoRepository.java
@@ -0,0 +1,7 @@
+package com.pruebareservas.repository;
+
+import com.pruebareservas.entity.PresupuestoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PresupuestoRepository extends JpaRepository<PresupuestoEntity, Long> {
+}
