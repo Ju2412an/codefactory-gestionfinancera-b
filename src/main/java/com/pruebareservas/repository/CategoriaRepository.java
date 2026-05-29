381ae1ae2acb0b49571a1bd03f12b1dfc1df35f1
ElAles19 <alexanderv1901@gmail.com>
2026-05-28 20:05:00 -0500

    Delete src/main/java/com/pruebareservas/repository directory

--- src/main/java/com/pruebareservas/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pruebareservas.repository;
-
-import com.pruebareservas.entity.CategoriaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<CategoriaEntity, Long> {
-}
